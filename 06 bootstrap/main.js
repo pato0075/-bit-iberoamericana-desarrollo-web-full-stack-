501de7f7b8f057aceba574f587584d7eaf89b06d
pato0075 <pato0075@gmail.com>
2022-04-19 19:12:18 -0500

    trabajo js

--- 06 bootstrap/main.js
+++ 06 bootstrap/main.js
@@ -1,41 +1,48 @@
-           
-           const validCredentials = {
-            name: 'vet@vet.com',
-            password: '4321',
-           };
-           
-           const name = document.getElementById('name');
-           const password = document.getElementById('password');
-           const submit = document.getElementById('btn');
-           
-           
-           const myCredentials = {
-            name: null,
-            password: null,
-           };
-           
-           const handleName = (event) => {
-           myCredentials.name = event.target.value; 
-           };
-           
-           const handlePassword = (event) => {
-           myCredentials.password = event.target.value;  
-           };
-           
-           const handleSubmit = () => {
-           const name = myCredentials.name === validCredentials.name;
-           const password = myCredentials.password === validCredentials.password;
-             
-             if  (name && password){
-                console.log(Bienvenido);
-             }  
-             else {
-               console.log(denegado);        
-             }
-           
-           
-          document.addEventListener('DOMContentLoaded', () => {
-            name.addEventListener('input', handleName);
-            password.addEventListener('input', handlePassword);
-           submit.addEventListener('click', handleSubmit);
-          });
+const validCredentials = {
+  name: "vet@vet.com",
+  password: "4321",
+};
+
+const $name = document.getElementById("name");
+const password = document.getElementById("password");
+const submit = document.getElementById("btn");
+const $privado = document.getElementById("privado");
+console.log($privado)
+
+const myCredentials = {
+  name: null,
+  password: null,
+};
+
+const handleName = (event) => {
+  console.log(event.target.value)
+  myCredentials.name = event.target.value;
+};
+
+const handlePassword = (event) => {
+  console.log(event.target.value)
+  myCredentials.password = event.target.value;
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const name = myCredentials.name === validCredentials.name;
+  const password = myCredentials.password === validCredentials.password;
+
+  if (name && password) {
+   console.log("Bienvenido");
+  /*  console.log($privado)
+  $privado.classList.remove("disabled");*/
+  window.open("nosotros.html");
+   
+   
+  } else {
+    console.log("denegado");
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  $name.addEventListener("input", handleName);
+  password.addEventListener("input", handlePassword);
+  submit.addEventListener("click", handleSubmit);
+});
