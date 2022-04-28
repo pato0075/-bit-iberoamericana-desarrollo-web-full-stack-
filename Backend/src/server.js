f044891ec7eaa29ec58a1e0ebc82484e9fdcf001
pato0075 <pato0075@gmail.com>
2022-04-27 22:16:17 -0500

    backend todos

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,6 +4,17 @@ const express = require ("express");
 //llamar mongoose
 const mongoose = require ("mongoose");
 
+//llamar la variable de entorno dotenv
+const dotenv = require ("dotenv");
+
+dotenv.config();
+
+//metodo para utilizar la variable de entorno process,env
+mongoose.connect(process.env.MONGODB_URI)
+.then(() =>console.log("se conecto con mongoDB:true"))
+.catch(()=> console.log("error de conexion:false"))
+
+
 const server = express()
 
 server.get("/", (req, res)=>{
@@ -13,11 +24,52 @@ server.get("/", (req, res)=>{
         res.send("un error")    
     }
 })
+
+//crud
+server.post('/api/v1/todos', (req, res)=>{
+    try {
+        res.send("Crear tarea")
+    } catch (error) {
+        res.send("un error")    
+    }
+})
+
+server.get('/api/v1/todos', (req, res)=>{
+    try {
+        res.send("Leer todas las tareas")
+    } catch (error) {
+        res.send("un error")    
+    }
+})
+
+server.get('/api/v1/todos:id', (req, res)=>{
+    try {
+        res.send("Leer una tarea especifica")
+    } catch (error) {
+        res.send("un error")    
+    }
+})
+
+server.put('/api/v1/todos:id', (req, res)=>{
+    try {
+        res.send("Actualizar una tarea especifica")
+    } catch (error) {
+        res.send("un error")    
+    }
+})
+
+server.delete('/api/v1/todos:id', (req, res)=>{
+    try {
+        res.send("Eliminar una tarea Especifica")
+    } catch (error) {
+        res.send("un error")    
+    }
+})
+
+
+
 server.listen (3000, ()=> console.log("servidor en el puerto 3000"))
 
 
-mongoose.connect(mongodb+srv://dandev82:<duckman80173306>@cluster0.we5tf.mongodb.net/bitdev82?retryWrites=true&w=majority));
-.then(() =>console.log("se conecto con mongoDB:true"))
-.catch (()=> console.log("error de conexion:false"))
 
 
